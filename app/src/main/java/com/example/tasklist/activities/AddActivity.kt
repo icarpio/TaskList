c127af401f882c4a504f0a086b1d7a5635913a54
icarpio <icarpiodeveloper@gmail.com>
2024-07-02 13:52:36 +0200

    Add AddActivity

--- app/src/main/java/com/example/tasklist/activities/AddActivity.kt
+++ app/src/main/java/com/example/tasklist/activities/AddActivity.kt
@@ -2,11 +2,30 @@ package com.example.tasklist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.tasklist.R
+import com.example.tasklist.data.Task
+import com.example.tasklist.data.TaskDAO
+import com.example.tasklist.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
+
+    private lateinit var taskDAO: TaskDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        taskDAO = TaskDAO(this)
+
+        binding.saveButton.setOnClickListener {
+            val taskName = binding.nameEditText.text.toString()
+            val task = Task(-1, taskName,false,"")
+            taskDAO.insertTask(task)
+            Toast.makeText(this, "Tarea guardad correctamente", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
